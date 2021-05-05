137f2e9030fbfcbf1ae8f94974233eda4d56cf33
Akshit saxena <007akshitsaxena@gmail.com>
2021-05-05 13:46:03 +0530

    added service type

---
+++ Models/ProductServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NBFC_App___dev.Models
+{
+    public class ProductServices
+    {
+       
+        public string name { get; set; }
+    }
+}
